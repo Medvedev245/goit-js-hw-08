544ae16895640f99d62ff90c39ede868e75f423d
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2023-08-12 09:38:50 +0200

    add desktop + 3 home task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+const FormData = {};
+
+refs.form.addEventListener('submit', formSubmit);
+refs.form.addEventListener('input', throttle(textareaInput, 500));
+
+manipulateText();
+
+//  1) Получаем значения TextArea и мейл
+//     кидаем єто в переменную FormData и в локалсторедж
+//     Сохраняем его в хранилище
+//     тротл 500
+
+function textareaInput(evt) {
+  FormData[evt.target.name] = evt.target.value;
+  console.log(FormData);
+  localStorage.setItem('feedback-form-state', JSON.stringify(FormData));
+}
+
+//   2) Убираем ПОУМОЛЧАНИЮ
+//      убираем сообщение из хранилища
+//      очищаем форму
+function formSubmit(evt) {
+  evt.preventDefault();
+  //   console.log('jnghfdrf');
+  evt.target.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+//   3) Получаем Значение из хранилища
+//      Если есть значение обновляем ДОМ
+function manipulateText() {
+  const saveText = localStorage.getItem('feedback-form-state');
+  if (saveText) {
+    refs.textarea.value = saveText;
+  }
+}
+
+console.log('werf');
+
+// ЮзерКейс Форма инпут меседж и емеил отправка через ЛокалСторедж
+// С Тротлем
